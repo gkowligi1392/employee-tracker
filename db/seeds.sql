a954a3d9f5728af61a6b590ff2b0a34d6d06a253
Gokul's Laptop <gkowligi@gmail.com>
2022-01-03 16:21:50 -0800

    add seed files

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO roles (role_name) VALUES 
+('Web Developer'),
+('Engineering'),
+('Database Engineering');
+
+
+INSERT INTO managers (manager_name) VALUES
+('John Manager'),
+('Johana Manager'),
+('Albert Manager');
---
+++ db/seeds_employees.sql
@@ -0,0 +1,4 @@
+INSERT INTO employees (first_name, last_name,salary, manager_id,role_id) VALUES 
+('Gokul','Kowligi',60000,1,1),
+('Ryan','Reynolds',60000,2,1),
+('Owen','Wilson',6000,1,1);

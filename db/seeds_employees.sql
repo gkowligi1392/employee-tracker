52a31e8f6ceb783ea46c7c335f60d4a02b0ddae6
Gokul's Laptop <gkowligi@gmail.com>
2022-01-03 16:32:24 -0800

    edited db schema

--- db/seeds_employees.sql
+++ db/seeds_employees.sql
@@ -1,4 +1,6 @@
-INSERT INTO employees (first_name, last_name,salary, manager_id,role_id) VALUES 
-('Gokul','Kowligi',60000,1,1),
-('Ryan','Reynolds',60000,2,1),
-('Owen','Wilson',6000,1,1);
+INSERT INTO employee (first_name, last_name, manager_id,role_id) VALUES 
+('Gokul','Kowligi',1,1),
+('Sivan','Pending',2,3),
+('Antonio','Huertas',3,5),
+('Jose','Velasquez',2,3),
+('Christian','Salcedo',3,6);
